1474ba4dec854ea743dbd4a6f13d170c6a05db58
Jim <jag.arnold.go@gmail.com>
2023-10-25 14:17:12 -0700

    add base converter and spec

--- lib/baser.rb
+++ lib/baser.rb
@@ -2,7 +2,48 @@
 
 require_relative "baser/version"
 
+# encode/decode base by the number of characters
 module Baser
   class Error < StandardError; end
-  # Your code goes here...
+
+  DEFAULT_CHARACTERS = ("0".."9").to_a + ("a".."z").to_a + ("A".."Z").to_a - %w[0 O I l]
+
+  class << self
+    attr_writer :characters
+
+    def characters
+      @characters ||= DEFAULT_CHARACTERS
+    end
+
+    def encode(number)
+      raise Error, "#{number} is not an Integer" unless number.is_a? Integer
+
+      str = ""
+      while number.positive?
+        str = characters[number % base] + str
+        number /= base
+      end
+      str
+    end
+
+    def decode(string)
+      raise Error, "#{string} is not a string" unless string.is_a? String
+
+      base10 = 0
+      string.reverse.each_char.with_index do |char, index|
+        base10 += hash[char] * (base**index)
+      end
+      base10
+    end
+
+    private
+
+    def base
+      @characters.length
+    end
+
+    def hash
+      Hash[characters.map.with_index.to_a]
+    end
+  end
 end
--- spec/baser_spec.rb
+++ spec/baser_spec.rb
@@ -1,11 +1,73 @@
 # frozen_string_literal: true
 
-RSpec.describe Baser do
+RSpec.describe Baser do # rubocop:disable Metrics/BlockLength
   it "has a version number" do
     expect(Baser::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "has a default characters" do
+    expect(Baser.characters).to eq Baser::DEFAULT_CHARACTERS
+  end
+
+  context "default characters" do
+    describe ".encode" do
+      it "converts to the default base" do
+        expect(Baser.encode(1200)).to eq "mG"
+      end
+      it "raise an error if not an integer" do
+        expect { Baser.encode("1200") }.to raise_error Baser::Error
+      end
+    end
+
+    describe ".decode" do
+      it "converts to the base 10" do
+        expect(Baser.decode("Fe12")).to eq 7_653_101
+      end
+      it "raise an error if not an integer" do
+        expect { Baser.decode(1200) }.to raise_error Baser::Error
+      end
+    end
+  end
+
+  context "custom characters" do
+    before do
+      Baser.characters = ("a".."z").to_a + ("A".."Z").to_a + ("0".."9").to_a
+    end
+    it "has a default characters" do
+      expect(Baser.characters).not_to eq Baser::DEFAULT_CHARACTERS
+    end
+
+    describe ".encode" do
+      it "converts to the default base" do
+        expect(Baser.encode(1200)).to eq "tw"
+      end
+    end
+
+    describe ".decode" do
+      it "converts to the base 10" do
+        expect(Baser.decode("Fe12")).to eq 7_406_884
+      end
+    end
+  end
+
+  context "custom characters" do
+    before do
+      Baser.characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".each_char.map { |c| c }
+    end
+    it "has a default characters" do
+      expect(Baser.characters).not_to eq Baser::DEFAULT_CHARACTERS
+    end
+
+    describe ".encode" do
+      it "converts to the default base" do
+        expect(Baser.encode(4589)).to eq "1c1"
+      end
+    end
+
+    describe ".decode" do
+      it "converts to the base 10" do
+        expect(Baser.decode("Hash")).to eq 10_288_297
+      end
+    end
   end
 end
