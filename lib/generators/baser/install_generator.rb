95b5da5bb4e27a70c2e78f6e1a6557332bc39dd4
Jim <jag.arnold.go@gmail.com>
2023-10-26 09:38:52 -0700

    fix source file for generators

--- lib/generators/baser/install_generator.rb
+++ lib/generators/baser/install_generator.rb
@@ -3,7 +3,7 @@
 module Baser
   # Execute generator to create file on initializer
   class InstallGenerator < Rails::Generators::Base
-    source_root File.expand_path("templates", __dir__)
+    source_root File.expand_path("../templates", __dir__)
     desc "This generator creates an baser.rb file at config/initializers"
     def copy_initializer_file
       copy_file "baser.rb", "config/initializers/baser.rb"
